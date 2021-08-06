b170b7099e53295babd07393a2565a3ad94e7428
Mohammed Hasnat <adamhash2@gmail.com>
2021-08-06 13:45:41 -0500

    implemented grouping objects by location prefix

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import {responseFromApi} from './data'
+import {  useState } from 'react';
 
 function App() {
+  const data=responseFromApi;
+  const [loc,setLoc]=useState("");
+  var objs=[{}];
+
+ 
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+      <input type="text" data-testid="input" placeholder="Type First two letters of the Location." onChange={event=>{setLoc(event.target.value)}}  />
+      <ul className="collection">
+      
+      {
+        data.data.dashboard.components.filter((val,key)=>{
+          if(loc==""){
+            objs=[{}]
+            return val
+          }else if((val.location.toLowerCase().substr(0,2))==loc.toLowerCase()){
+            objs.push({location:val.location,obj:data.data.dashboard.components[key]} )
+            console.log(objs)
+            return val
+          }
+        }).map((val,key)=>{
+
+          return <li className="collection-item" key={key} style={{padding:10,backgroundColor:"#bfe6ff",margin:10}}>{val.location} </li>
+
+        })
+      }
+    </ul>
+
     </div>
   );
 }
