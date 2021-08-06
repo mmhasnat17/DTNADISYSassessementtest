729c36fd6da5a16d85355db1036af2a6c4071a19
Mohammed Hasnat <adamhash2@gmail.com>
2021-08-06 13:47:47 -0500

    two unit test are added -  testing component render and checking input field and data is present on the screen

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
-import { render, screen } from '@testing-library/react';
+import { getByTestId, render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/AGAAA/);
   expect(linkElement).toBeInTheDocument();
 });
+
+it("testing the input field",()=>{
+  const input=document.getElementsByTagName('input').val;
+  expect(input=="");
+})
